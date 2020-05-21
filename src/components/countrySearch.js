079e801f52803fc16eaca0e1a135885d6c8a6d88
Kenneth Ofosu <kofosu89@gmail.com>
2020-05-21 15:22:13 -0400

    Limit country downdrop list to visible regions Filtered country dropdown list to visible regions only. Also set Dropdown list to hidden during active quizzes.

--- src/components/countrySearch.js
+++ src/components/countrySearch.js
@@ -6,14 +6,19 @@ import { geoPath } from "d3-geo"
 export default class CountrySearch extends Component {
   render() {
     let countries = this.props.state.geographyPaths
+    if(this.props.state.currentMap !== "world") {
+      countries = countries.filter(x => this.props.state.filterRegions.includes(x.properties.alpha3Code))
+    }
+
+    countries = countries
       .map(x => {
-        if (!x.properties.alpha2Code) { return null };
+        if(!x.properties.alpha2Code) {return null};
         let y = x.properties.alpha2Code.toString().toLowerCase()
-        return {key: y, flag: y, text: x.properties.name, value: x.properties.alpha3Code}
+        return { key: y, flag: y, text: x.properties.name, value: x.properties.alpha3Code}
       })
       .filter(x => x !== null)
-      .filter(x => !['bl', 'cw', 'gg', 'im', 'je', 'mf', 'ss', 'sx'].includes(x.key))
-      .sort((a, b) => a.text > b.text ? 1 : -1)
+      .filter(x => !['bl','cw','gg','im','je','mf','ss','sx'].includes(x.key))
+      .sort((a,b) => a.text > b.text ? 1:-1)
     
     return (
       <div className="countrySearch">
